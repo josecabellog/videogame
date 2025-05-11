e23e2afd7278715e42425afd2fc2a916faf8a515
josecabellog <josegregoriocabellog@gmail.com>
2025-05-10 21:28:36 -0400

    css added

--- homepage/urls.py
+++ homepage/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('categories/', CategoryPageView.as_view(), name='categories'),
-    path('contact/', ContactPageView.as_view(), name='contect'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
 ]
