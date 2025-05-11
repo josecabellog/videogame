464bf66b739dad823862377619643745b0e78bd3
josecabellog <josegregoriocabellog@gmail.com>
2025-05-10 20:34:10 -0400

    venv completed

---
+++ homepage/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import HomePageView, AboutPageView, CategoryPageView, ContactPageView
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('categories/', CategoryPageView.as_view(), name='categories'),
+    path('contact/', ContactPageView.as_view(), name='contect'),
+]
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'index.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+class CategoryPageView(TemplateView):
+    template_name = 'categories.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
